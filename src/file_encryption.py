7c9e733a50e7ac2daf070795346a431ca324a816
Ivan Goncharuk <ivangoncharuk.biz@gmail.com>
2023-12-19 01:18:17 -0500

    file encryption now uses a file dialogue

--- src/file_encryption.py
+++ src/file_encryption.py
@@ -5,6 +5,8 @@ from cryptography.hazmat.primitives import serialization
 from InquirerPy import prompt
 from rich.console import Console
 import os
+import tkinter as tk
+from tkinter import filedialog
 
 console = Console()
 
@@ -74,39 +76,49 @@ def file_encryption_menu():
     Display the file encryption/decryption menu and handle user interactions.
     """
     choices = {"encrypt": "Encrypt a file", "decrypt": "Decrypt a file"}
-    questions = [
-        {
-            "type": "list",
-            "name": "action",
-            "message": "Select an action:",
-            "choices": list(choices.values()),
-        }
-    ]
-    action = prompt(questions)["action"]
-
-    file_path_question = {
-        "type": "input",
-        "name": "file_path",
-        "message": "Enter the path of the file:",
-    }
-    file_path = prompt([file_path_question])["file_path"]
-
-    key_path_question = {
-        "type": "input",
-        "name": "key_path",
-        "message": "Enter the path of the key:",
-    }
-    key_path = prompt([key_path_question])["key_path"]
+    action = prompt(
+        [
+            {
+                "type": "list",
+                "name": "action",
+                "message": "Select an action:",
+                "choices": list(choices.values()),
+            }
+        ]
+    )["action"]
 
     if action == choices["encrypt"]:
-        public_key = load_key(key_path, is_private=False)
-        encrypted_file_path = encrypt_file(file_path, public_key)
-        console.print(
-            f"File encrypted successfully. Encrypted file: {encrypted_file_path}"
-        )
+        file_path = select_file("Select a file to encrypt")
+        key_path = select_key_file(is_private=False)
+        if file_path and key_path:
+            public_key = load_key(key_path, is_private=False)
+            encrypted_file_path = encrypt_file(file_path, public_key)
+            console.print(
+                f"File encrypted successfully. Encrypted file: {encrypted_file_path}"
+            )
     elif action == choices["decrypt"]:
-        private_key = load_key(key_path, is_private=True)
-        decrypted_file_path = decrypt_file(file_path, private_key)
-        console.print(
-            f"File decrypted successfully. Decrypted file: {decrypted_file_path}"
-        )
+        file_path = select_file("Select a file to decrypt")
+        key_path = select_key_file(is_private=True)
+        if file_path and key_path:
+            private_key = load_key(key_path, is_private=True)
+            decrypted_file_path = decrypt_file(file_path, private_key)
+            console.print(
+                f"File decrypted successfully. Decrypted file: {decrypted_file_path}"
+            )
+
+
+def select_file(title="Select a file"):
+    """
+    Open a file dialog for the user to select a file using tkinter.
+    """
+    root = tk.Tk()
+    root.withdraw()  # Hide the main tkinter window
+    return filedialog.askopenfilename(title=title)
+
+
+def select_key_file(is_private):
+    """
+    Open a file dialog for the user to select a key file using tkinter.
+    """
+    title = "Select a Private Key" if is_private else "Select a Public Key"
+    return select_file(title)
